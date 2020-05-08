12d46a2fd4d41d2ae3e018ad5d5accc18748d047
Jason Plourde <jasojo97@gmail.com>
2020-05-08 13:07:23 -0700

    updated getting descriptions

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -52,7 +52,7 @@ export class StoreComponent {
   }
 
   changeCategory(newCategory?: string) {
-    // console.log('change categorhy. New value: ' + newCategory)
+    
     this.selectedCategory = newCategory
   }
 
@@ -69,11 +69,10 @@ export class StoreComponent {
     const products = this.repository.getProducts(this.selectedCategory)
     const pages = products.length / this.productsPerPage
 
-    // the next whole number, 0.5 => 1, 3.2 => 4
+   
     const pageCount = Math.ceil(pages)
 
-    // x is 0 and i is the index
-    // if page count is 3, it generate [1, 2, 3]
+    
     const pageNumbers = Array(pageCount)
       .fill(0) // fills with 0
       .map((x, i) => i + 1)
